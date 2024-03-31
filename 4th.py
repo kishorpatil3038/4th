ba1f2fbc32564e04069ffb70468f5dc558f78fca
kishorpatil3038 <gurukrupa3038@gmail.com>
2024-03-31 22:30:11 +0530

    this is my 4th repo

---
+++ 4th.py
@@ -0,0 +1,11 @@
+
+a = 32
+b = 28
+c = 155
+
+if a > b and a > c :
+    print(a , "is greatre then", b,"and",c)
+elif b > c and b > a:
+    print(b ,"is greater the", a, "and", c)
+elif c > a and c > b:
+    print(c, "is greater then", a, "and", b)
